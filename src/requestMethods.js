2598dec140caa9b07b24178c7506a2d37176f557
Tiwa-C <87069695+Tiwa-C@users.noreply.github.com>
2022-03-29 16:29:06 +1100

    Update requestMethods.js
    Edit TOKEN

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTlmNTgyNjRiYmRlYmVmN2RlMmE2NSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NjE5OTY0NywiZXhwIjoxNjQ2NDU4ODQ3fQ.nyhY9pI-WekCameHMClkAbtE26aWcB7IxVTeQqnJne4";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
